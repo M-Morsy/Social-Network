1f36d086727f53f96a12b954fce62e3ec88f6c0a
magedmabrouk <magedmabrouk@ymail.com>
2018-04-20 16:07:59 +0200

    Add files via upload

---
+++ Code/post test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 15:38:07 2018
+
+@author: maged
+"""
+
+from Post import *
+post1 = Post('yalla nro7 el so5na', 125)
+post1.AddComment('la fakss', 36)
+post1.AddComment('msh fady', 98)
+post1.AddComment('lih yagd3aaan!!', 125)
+post1.AddComment('fih final bokra!', 98)
+post1.AddComment('ya 7ywan ya mot5lf.. enta yatrash', 36)
+
+post2 = Post('el so7ab fi agaza', 125)
+
+print(post1.Post_view())
+print(post2.Post_view())
