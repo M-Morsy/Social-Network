1f6d4188b401f8d71c18a8ba092411155f9d025e
M-Morsy <mohamed.morsey96@gmail.com>
2018-04-20 15:48:34 +0200

    Edit in testing, import related

--- Code/Person_Test.py
+++ Code/Person_Test.py
@@ -1,21 +1,21 @@
 from Code import Person
-from Person import *
+# from Person import *
 
-p1 = Person("ahmed", "hamada@gmail.com", "ahm;ol")
+p1 = Person.Person("ahmed", "hamada@gmail.com", "ahm;ol")
 print(p1.get_ID())
 print(p1.get_name())
 print(p1.get_email())
 print(p1.get_password())
 print(p1.get_info())
 
-p2 = Person ("hamda" , "ham@gmail.com" , "hamadaa25")
+p2 = Person.Person("hamda", "ham@gmail.com", "hamadaa25")
 print(p2.get_email())
 print(p2.get_ID())
 print(p2.get_name())
 print(p2.get_password())
 print(p2.get_info())
 
-print ("Count = ", Person.Count)
+print("Count = ", Person.Person.Count)
 
 # print (Person.name) >> error
 # print(Person.dict)
