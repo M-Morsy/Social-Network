ec2bb00d0f81b0e9993f5edb6c85f9d724b06361
magedmabrouk <magedmabrouk@ymail.com>
2018-04-20 15:26:26 +0200

    Update Post.py

--- Code/Post.py
+++ Code/Post.py
@@ -55,15 +55,3 @@ class Comment:
     def edit (self, text):
         self.text = text
 
-
-post1 = Post('yalla nro7 el so5na', 125)
-post1.AddComment('la fakss', 36)
-post1.AddComment('msh fady', 98)
-post1.AddComment('lih yagd3aaan!!', 125)
-post1.AddComment('fih final bokra!', 98)
-post1.AddComment('ya 7ywan ya mot5lf.. enta yatrash', 36)
-
-post2 = Post('el so7ab fi agaza', 125)
-
-print(post1.Post_view())
-print(post2.Post_view())
