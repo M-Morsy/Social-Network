b752ad9f4da81b4f14e58025209457da3700e69e
Shimaa Hassan <eng.shimaa71296@gmail.com>
2018-04-21 00:08:53 +0200

    edit class mistakes

--- Code/Users.py
+++ Code/Users.py
@@ -1,15 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 15:05:17 2018
+
+@author: shimaa
+"""
 class Users(): 
   #** Data **##
   numUsers = 0  # Actual number of users in the site 
   maxUsers = 0 # Max number of users in the site we can deal with
   Users = [0 for x in range(maxUsers)] # all users
-  edges = [[0 for x in range(maxUsers)] for y in range(maxUsers)] # all connection between users
+  edges = [] #[[0 for x in range(maxUsers)] for y in range(maxUsers)] # all connection between users
   marks = [0 for x in range(maxUsers)] # mark user when search about special one
   ##** Constructor & Destrcutor **##
   def __init__(self, maxUsers): # set the value of max users
         self.maxUsers     = maxUsers 
+        for row in range(self.maxUsers):
+          self.edges.append([])
+          for col in range(self.maxUsers):
+             self.edges[row].append(0)       
   def __del__(self): # nothing to do 
-       print ("users: ")
+       print ("users: ",self.Users)
   ##** Graph functionality **##
   def MakeEmpty(self): # delete every thing
       self.numUsers = 0
@@ -34,6 +46,7 @@ class Users():
       return
   def AddUser(self,person): # add new user
       self.Users.append(person)
+      self.marks.append(0)
       self.numUsers += 1
   def AddEdge(self,fromPesron, toPerson, weight): # add new connection
       row = self.IndexIs(fromPesron)
@@ -46,8 +59,8 @@ class Users():
   def GetToUsers(self,pesron): #for searching algorithm
       print("nothing")
   def ClearMarks(self): # delete all marks
-      del self.marks
-      self.marks = [0 for x in range(self.maxUsers)]
+      for i in range(self.numUsers):
+          self.marks[i] = 0
   def MarkUser(self,pesron): # mark user to ignore it in searching 
       index = self.IndexIs(pesron)
       self.marks[index] = 1
