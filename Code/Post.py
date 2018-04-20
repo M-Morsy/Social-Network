191f27045678cd19fbf10838219c05e7355ed45f
magedmabrouk <magedmabrouk@ymail.com>
2018-04-20 15:14:15 +0200

    Update Post.py

--- Code/Post.py
+++ Code/Post.py
@@ -1,29 +1,69 @@
 class Post:
     Post_ID = 0
+   
     def __init__ (self, text, user_ID):
         self.text = text
         self.user_ID = user_ID
         Post.Post_ID += 1
         self.Post_ID = Post.Post_ID
         self.Comments=[]
-        self.CommentCount = 0
+    def get_Post_ID (self):
+        return self.Post_ID
+    def set_Post_ID (self, Post_ID):
+        self.Post_ID = Post_ID
+    def get_user_ID (self):
+        return self.user_ID
+    def get_text(self):
+        return self.text
     def edit (self, text):
         self.text = text
     def AddComment(self, text, user_ID):
-        self.CommentCount +=1
-        self.Comments.append(Comment(text, user_ID, self.Post_ID, self.CommentCount))
+        self.Comments.append(Comment(text, user_ID, self.Post_ID))
     def editComment(self, text, CommentCount):
-        self.Comments[CommentCount].text = text
+        self.Comments[CommentCount].edit(text)
     def deleteComment(self, CommentCount):
         del self.Comments[CommentCount]
+    def get_Comments_num(self):
+        return len(self.Comments)
+    def Post_view (self):
+        Post_contents = "{}\n \n".format(self.text)
+        i = 1
+        for x in self.Comments:
+            Post_contents += "     {}- {}: {} \n \n".format(i, x.user_ID, x.text)
+            i += 1
+        return Post_contents
     
 class Comment:
     Comment_ID = 0
-    def __init__ (self, text, user_ID, Post_ID, CommentCount):
+
+    def __init__ (self, text, user_ID, Post_ID):
         self.text = text 
         self.user_ID = user_ID
         self.Post_ID = Post_ID
         Comment.Comment_ID += 1
         self.Comment_ID = Comment.Comment_ID
-        self.CommentCount = CommentCount
-        
+    def get_text(self):
+        return self.text
+    def get_Post_ID(self):
+        return self.Post_ID
+    def get_Comment_ID(self):
+        return self.Comment_ID
+    def set_Comment_ID(self, Comment_ID):
+        self.Comment_ID = Comment_ID
+    def get_user_ID(self):
+        return self.user_ID
+    def edit (self, text):
+        self.text = text
+
+
+post1 = Post('yalla nro7 el so5na', 125)
+post1.AddComment('la fakss', 36)
+post1.AddComment('msh fady', 98)
+post1.AddComment('lih yagd3aaan!!', 125)
+post1.AddComment('fih final bokra!', 98)
+post1.AddComment('ya 7ywan ya mot5lf.. enta yatrash', 36)
+
+post2 = Post('el so7ab fi agaza', 125)
+
+print(post1.Post_view())
+print(post2.Post_view())
