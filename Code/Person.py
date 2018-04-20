d28ab852782da8295d59b2b2c4d5606d99d8fc15
M-Morsy <mohamed.morsey96@gmail.com>
2018-04-20 15:21:54 +0200

    No dict. Tested V1

--- Code/Person.py
+++ Code/Person.py
@@ -1,88 +1,116 @@
-class person ():
-    #** Data **##
+class Person:
+    # ** Data ** ##
+    """
     dict = {
-                "ID"       : [], # generated
-                "Name"     : [], # Just user name not sir name
-                "Email"    : [], # Normal Email
-                "Password" : [] # four digits password = 10^4 users    
+                "ID": [],  # generated
+                "Name": [],  # Just user name not sir name
+                "Email": [],  # Normal Email
+                "Password": []  # four digits password = 10^4 users
             }
-    Posts  = [] # Posts'  IDs >> Linked list >> To Be Done
-    Groups = [] # groups' IDs >> Linked list >> To Be Done
-    Admin  = [] # groups' IDs >> Linked list >> To Be Done
+    """
+    Posts = []   # Posts'  IDs >> Linked list >> To Be Done
+    Groups = []  # groups' IDs >> Linked list >> To Be Done
+    Admin = []   # groups' IDs >> Linked list >> To Be Done
     next = None
     Count = 0
     
-    ##** Constructor & Destrcutor **##
-    def __init__(self, Name , Email , Password):
-        self.dict['ID']       = 0 + Count # To be replaced by random ID
-        self.dict['Name']     = Name
-        self.dict['Email']    = Email
-        self.dict['Password'] = Password
-        Count += 1
+    # ** Constructor & Destructor **##
+    def __init__(self, name, email, password):
+        Person.Count += 1
+        self.ID = self.Count
+        self.name = name
+        self.email = email
+        self.password = password
+        # self.dict['ID'].append(self.Count)  # To be replaced by random ID
+        # self.dict['Name'].append(name)
+        # self.dict['Email'].append(email)
+        # self.dict['Password'].append(password)
 
     def __del__(self):
-        print ("person: " , self.dict['Name'] , "with ID: ", self.dict['ID'] , "died")
-    ##** Getters and Setters **##
-    def get_ID (self):
-        return self.dict['ID']
-    def set_ID (self , ID):
-        self.dict['ID'] = ID   
-    def get_Name (self):
-        return self.dict['Name']
-    def get_Name (self , Name):
-        self.dict['Name'] = Name   
-    def get_Email (self):
-        return self.dict['Email']
-    def set_Email (self , Email):
-        self.dict['Email'] = Email
-    def get_Password (self):
-        return self.dict['Password']
-    def get_Password (self , Password):
-        self.dict['Password'] = Password
-    ##** getters and setters for whole information **##
-    def get_info (self):
-        return [self.dict['ID'] , self.dict['Name'] , self.dict['Email'] , self.dict['Password']]
-    def set_info (self , ID = None , Name = None , Email = None , Password = None):
+        print ("person: ", self.name, "with ID: ", self.ID, "died")
+    # ** Getters and Setters **##
+
+    def get_ID(self):
+        return self.ID
+
+    def set_ID(self, ID):
+        self.ID = ID
+        # self.dict['ID'].append(ID)
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+        # self.dict['Name'].append(name)
+
+    def get_email(self):
+        return self.email
+
+    def set_email(self, email):
+        self.email = email
+        # self.dict['Email'].append(email)
+
+    def get_password(self):
+        return self.password
+
+    def set_password (self, password):
+        self.password = password
+        # self.dict['Password'].append(password)
+
+    # ** getters and setters for whole information **##
+    def get_info(self):
+        return [self.ID, self.name, self.email , self.password]
+
+    def set_info(self, ID=None, name=None, email=None, password=None):
         if ID is None:
             pass
         else:
-            self.dict['ID'] = ID
-        if Name is None: 
+            self.set_ID(ID)
+        if name is None:
             pass
         else:
-            self.dict['Name'] = Name
-        if Email is None:
+            self.set_name(name)
+        if email is None:
             pass
         else:
-            self.dict['Email'] = Email
-        if Password is None:
+            self.set_email(email)
+        if password is None:
             pass
         else:
-            self.dict['Password'] = Password   
-    
-    ##** adding **##
-    def add_post (self , Post_ID):
-        self.Posts.append(Post_ID)
-    def add_Group (self , Group_ID):
-        self.Groups.append(Group_ID)
-    def Add_Admin_Group (self , Group_ID):
-        self.Admin.append(Group_ID)
+            self.set_password(password)
+
+    # ** adding **##
+    def add_post(self, post_ID):
+        self.Posts.append(post_ID)
+
+    def add_group (self, group_ID):
+        self.Groups.append(group_ID)
+
+    def add_admin_group (self, group_ID):
+        self.Admin.append(group_ID)
         
     # when a user is declared Admin, he will be declared here and in groups class or its equivilant  
     
-    ##** removing **##
-    def remove_post (self , Post_ID):
-        if (self.Posts.index(Post_ID)):
-            self.Posts.remove(Post_ID)
+    # ** removing **##
+    def remove_post (self , post_ID):
+        if (self.Posts.index(post_ID)):
+            self.Posts.remove(post_ID)
         else:
-            print ("error") # TBR
-    def remove_Group (self , Group_ID):
-        if (self.Groups.index(Group_ID)):
-            self.Groups.remove(Group_ID)
+            print ("error")  # TBR
+
+    def remove_group (self, group_ID):
+        if (self.Groups.index(group_ID)):
+            self.Groups.remove(group_ID)
         else:
-            print ("error") # TBR
-    def remove_Admin_Group (self , Group_ID):
-        if (self.Admin.index(Group_ID)):
-            self.Admin.remove(Group_ID)
+            print ("error")  # TBR
+
+    def remove_admin_group (self, group_ID):
+        if (self.Admin.index(group_ID)):
+            self.Admin.remove(group_ID)
         else:
-            print ("error") # TBR
+            print ("error")  # TBR
+
+
+
+
