02297d61afbf9e67df5116b1b9e4ad02400468ae
M-Morsy <mohamed.morsey96@gmail.com>
2018-05-02 22:47:28 +0200

    post and comment linked - added time

--- Code/Post.py
+++ Code/Post.py
@@ -1,10 +1,51 @@
-class Post:
+from datetime import datetime
+import Person as person
+class Comment:
+    comment_id = 0
+
+    def __init__ (self, text, user_id, post_id):
+        self.text = text
+        self.user_id = user_id
+        self.post_id = post_id
+        self.time = datetime.now()
+        self.comment_id = Comment.comment_id
+        Comment.comment_id += 1
+
+    def __del__(self):
+        pass
+
+    def get_text(self):
+        return self.text
+
+    def get_post_id(self):
+        try:
+            return self.post_id
+        except:
+            print("access error")
+
+    def get_comment_id(self):
+        return self.comment_id
+
+    def get_comment_time(self):
+        return self.time
+
+    def set_comment_id(self, comment_id):
+        self.comment_id = comment_id
+
+    def get_user_id(self):
+        return self.user_id
+
+    def edit(self, text):
+        self.text = text
+
+class Post(Comment, person.Person):
     post_count = 0
    
     def __init__ (self, text, user_id):
         self.text = text
         self.user_id = user_id
         self.post_id = Post.post_count
+        self.time = str(datetime.now())
         Post.post_count += 1
         self.Comments = []
 
@@ -26,27 +67,41 @@ class Post:
     def edit (self, text):
         self.text = text
 
-    def add_comment(self, text, user_id):
+    def get_time(self):
+        return  self.time
+
+    def add_comment (self, text, user_id):
+        '''to be done after Soha's task: 30 user scenario'''
+        #if(person.Person.Count < user_id):
+         #   print("not a valid user")
+        #else:
+         #   self.Comments.append(Comment(text, user_id, self.post_id))
         self.Comments.append(Comment(text, user_id, self.post_id))
 
+
     def edit_comment(self, text, comment_count):
-        self.Comments[comment_count].edit(text)
+        try:
+            self.Comments[comment_count].edit(text)
+        except:
+            print("Error in accessing the list")
 
     def delete_comment(self, comment_count):
         del self.Comments[comment_count]
 
-    # not working
-    '''
     def get_comment_id(self, text):
-        return self.Comments.index(text)
-    '''
+        for i in range(self.Comments.__len__()):
+            if self.Comments[i].get_text() == text:
+                return i
 
-    def get_comments_num(self):
+    def get_comments_num(self):     # return local comments number
         return len(self.Comments)
+
+    def get_comment_time(self, comment_id):
+        return self.Comments[comment_id].get_comment_time()
 
     def post_view(self):
         post_contents = "{}\n \n".format(self.text)
-        i = 1
+        i = 0
         for x in self.Comments:
             post_contents += "     {}- {}: {} \n \n".format(i, x.user_id, x.text)
             i += 1
@@ -54,34 +109,5 @@ class Post:
         return post_contents
 
 
-class Comment:
-    comment_id = 0
 
-    def __init__ (self, text, user_id, post_id):
-        self.text = text 
-        self.user_id = user_id
-        self.post_id = post_id
-        Comment.comment_id += 1
-        self.comment_id = Comment.comment_id
-
-    def __del__(self):
-        pass        
-
-    def get_text(self):
-        return self.text
-
-    def get_post_id(self):
-        return self.post_id
-
-    def get_comment_id(self):
-        return self.comment_id
-
-    def set_comment_id(self, comment_id):
-        self.comment_id = comment_id
-
-    def get_user_id(self):
-        return self.user_id
-
-    def edit(self, text):
-        self.text = text
 
--- Code/post test.py
+++ Code/post test.py
@@ -6,18 +6,48 @@ Created on Fri Apr 20 15:38:07 2018
 """
 
 from Post import *
+
 post1 = Post('yalla nro7 el so5na', 125)
+post1.add_comment('mashy', 8)
 post1.add_comment('la fakss', 36)
 post1.add_comment('msh fady', 98)
 post1.add_comment('lih yagd3aaan!!', 125)
 post1.add_comment('fih final bokra!', 98)
 post1.add_comment('ya 7ywan ya mot5lf.. enta yatrash', 36)
-
+post1.edit_comment("edited comment", 3)
 post2 = Post('el so7ab fi agaza', 125)
+post2.add_comment ("let's see", 200)
 print(post2.get_text())
 print(post1.post_view())
 print(post2.post_view())
 print(post1.get_comments_num())
+print()
+# integration between comment and post
+print("TESTING INTEGRATION BETWEEN POST AND COMMENT:")
+print("----------------------------------------------")
+print("comment 0 in post1 text: ", post1.Comments[0].get_text())
+print("comment 1 in post1 global id: ", post1.Comments[1].get_comment_id())
+print("comment 1 in post1 local id: ", post1.get_comment_id('la fakss'))
+print("comment 0 in post2 global id: ", post2.Comments[0].get_comment_id())
+print("comment 0 in post2 local id: ", post2.get_comment_id("let's see"))
+
+
 # get id of a comment
-# print(post1.Comments[3].get_comment_id)
+print(post1.Comments[3].get_comment_id)
 # print(post1.get_comment_id('ya 7ywan ya mot5lf.. enta yatrash'))
+print()
+# ** get post time ** #
+print("TESTING THE TIME ATTRIBUTE: ")
+print("------------------------------")
+print("first post time is: ", post1.time)
+print("second post time is: ", post2.time)
+print()
+print("TESTING TIME COMPARISONS: ")
+print("------------------------------")
+print("post1.time > post2.time: ", post1.time > post2.time)
+print("post1.time < post2.time: ", post1.time < post2.time)
+print("post1.time == post2.time:", post1.time == post2.time)
+print("type of time is: ", type(post1.time))
+
+print("comment 0 on post 1 time with local id: ", post1.get_comment_time(0))  # use local comment id here
+print("comment 0 on post 2 time with local id: ", post2.get_comment_time(0))
