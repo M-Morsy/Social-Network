4a9446ad12f7390cd55ec645df93ad4cb8c6ac66
SohaAlaa <32998187+SohaAlaa@users.noreply.github.com>
2018-05-14 00:33:51 +0200

    Add files via upload

---
+++ Code/posts&comm no-ids.py
@@ -0,0 +1,108 @@
+admin.add_post(post.Post("Set your goals high, and don’t stop till you get there", 0), 1 )
+admin.Posts[0].add_comment("Motivation will almost always beat mere talent", 2)
+
+
+
+admin.add_post(post.Post("It’s always too early to quit", post id), 3 )
+admin.Posts[post id].add_comment("With self-discipline most anything is possible", 4)
+
+
+admin.add_post(post.Post("Out of difficulties grow miracles", post id), 5 )
+admin.Posts[post id].add_comment("my first comment", 6)
+
+
+
+admin.add_post(post.Post("The purpose of our lives is to be happy", post id), 7 )
+admin.Posts[post id].add_comment("If you have never failed you have never lived", 8)
+
+
+admin.add_post(post.Post("A jug fills drop by drop", post id), 9 )
+admin.Posts[post id].add_comment("The harder I work, the luckier I get", 10)
+
+
+
+admin.add_post(post.Post("If there is no struggle, there is no progress", post id), 11 )
+admin.Posts[post id].add_comment("Believe and act as if it were impossible to fail", 12)
+
+
+admin.add_post(post.Post("Dream big and dare to fail", post id), 13 )
+admin.Posts[post id].add_comment("Turn your wounds into wisdom", 14)
+
+
+
+admin.add_post(post.Post("Great ideas often receive violent opposition from mediocre minds Albert Einstein", post id), 15 )
+admin.Posts[post id].add_comment("Don’t count the days, make the days count.” – Muhammad Ali", 16)
+
+
+admin.add_post(post.Post("Either I will find a way, or I will make one", post id), 17 )
+admin.Posts[post id].add_comment("After a storm comes a calm", 18)
+
+
+
+admin.add_post(post.Post("Your big opportunity may be right where you are now Napoleon Hill", post id), 19)
+admin.Posts[post id].add_comment("Whoever is happy will make others happy too", 20)
+
+
+admin.add_post(post.Post("Never, never, never give up", post id), 21 )
+admin.Posts[post id].add_comment("Wherever you go, go with all your heart", 22)
+
+
+
+admin.add_post(post.Post("Wherever you go, go with all your heart", post id), 23 )
+admin.Posts[post id].add_comment("Problems are not stop signs, they are guidelines", 24)
+
+
+admin.add_post(post.Post("Things do not happen. Things are made to happen", post id), 25 )
+admin.Posts[post id].add_comment("Even if you fall on your face, you’re still moving forward", 26)
+
+
+
+admin.add_post(post.Post("The dumbest people I know are those who know it all", post id), 27 )
+admin.Posts[post id].add_comment("Do something wonderful, people may imitate it", 28)
+
+
+admin.add_post(post.Post("Life is short. Build stuff that matters", post id), 29 )
+admin.Posts[post id].add_comment("Everything’s got a moral, if only you can find it", 30)
+
+
+
+admin.add_post(post.Post("When I let go of what I am, I become what I might be", post id), 31 )
+admin.Posts[post id].add_comment("If you want to lift yourself up, lift up someone else", 32)
+
+
+admin.add_post(post.Post("Once you choose hope, anything’s possible", post id), 33 )
+admin.Posts[post id].add_comment("Don’t regret the past, just learn from it", 34)
+
+
+
+admin.add_post(post.Post("Live what you love", post id), 35 )
+admin.Posts[post id].add_comment("All things are possible if you believe", 36)
+
+
+admin.add_post(post.Post("What consumes your mind, controls your life", post id), 37 )
+admin.Posts[post id].add_comment("You are stronger then you think", 38)
+
+
+
+admin.add_post(post.Post("Hope is a waking dream", post id), 39 )
+admin.Posts[post id].add_comment("You miss 100% of the shots you don’t take", 40)
+
+
+admin.add_post(post.Post("The mind is everything. What you think you become", post id), 40 )
+admin.Posts[post id].add_comment("Dream big and dare to fail", 39)
+
+
+
+admin.add_post(post.Post("Do one thing every day that scares you", post id), 33 )
+admin.Posts[post id].add_comment("Tough times never last, but tough people do", 32)
+
+
+admin.add_post(post.Post("Live each day as if your life had just begun", post id), 29 )
+admin.Posts[post id].add_comment("Education costs money. But then so does ignorance", 27)
+
+
+
+
+
+
+
