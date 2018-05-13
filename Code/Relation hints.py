e8fd97154fe6af9fd7563c712f6c1797793728ea
SohaAlaa <32998187+SohaAlaa@users.noreply.github.com>
2018-05-13 22:55:05 +0200

    Add files via upload

---
+++ Code/Relation hints.py
@@ -0,0 +1,159 @@
+admin.add_relation(sender_id=p1.get_id(), receiver_id=p2.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p1.get_id(),p2.get_id())
+   
+
+
+admin.add_relation(sender_id=p2.get_id(), receiver_id=p7.get_id(), weight=users.relation.Friend)
+   admin.accept_relation(p2.get_id(),p7.get_id())
+   
+
+admin.add_relation(sender_id=p7.get_id(), receiver_id=p6.get_id(), weight=users.relation.Child)
+   admin.accept_relation(p7.get_id(),p6.get_id())
+   
+
+admin.add_relation(sender_id=p6.get_id(), receiver_id=p1.get_id(), weight=users.relation.Relative)
+   admin.accept_relation(p6.get_id(),p1.get_id())
+   
+
+admin.add_relation(sender_id=p2.get_id(), receiver_id=p8.get_id(), weight=users.relation.Sibling)
+   admin.accept_relation(p2.get_id(),p8.get_id())
+   
+
+admin.add_relation(sender_id=p3.get_id(), receiver_id=p8.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p3.get_id(),p8.get_id())
+   
+
+admin.add_relation(sender_id=p8.get_id(), receiver_id=p4.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p8.get_id(),p4.get_id())
+   
+
+admin.add_relation(sender_id=p4.get_id(), receiver_id=p10.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p4.get_id(),p10.get_id())
+  
+
+admin.add_relation(sender_id=p5.get_id(), receiver_id=p10.get_id(), weight=users.relation.Child)
+   admin.accept_relation(p5.get_id(),p10.get_id())
+  
+
+admin.add_relation(sender_id=p9.get_id(), receiver_id=p5.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p9.get_id(),p5.get_id())
+   
+
+admin.add_relation(sender_id=p6.get_id(), receiver_id=p11.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p6.get_id(),p11.get_id())
+   
+
+admin.add_relation(sender_id=p7.get_id(), receiver_id=p12.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p7.get_id(),p12.get_id())
+   
+
+admin.add_relation(sender_id=p8.get_id(), receiver_id=p13.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p8.get_id(),p13.get_id())
+   
+
+admin.add_relation(sender_id=p14.get_id(), receiver_id=p9.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p14.get_id(),p9.get_id())
+   
+
+admin.add_relation(sender_id=p10.get_id(), receiver_id=p15.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p10.get_id(),p15.get_id())
+   
+
+admin.add_relation(sender_id=p11.get_id(), receiver_id=p20.get_id(), weight=users.relation.Sibling)
+   admin.accept_relation(p11.get_id(),p20.get_id())
+   
+
+admin.add_relation(sender_id=p12.get_id(), receiver_id=p16.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p12.get_id(),p16.get_id())
+   
+
+admin.add_relation(sender_id=p13.get_id(), receiver_id=p17.get_id(), weight=users.relation.Child)
+   admin.accept_relation(p13.get_id(),p17.get_id())
+   
+
+admin.add_relation(sender_id=p14.get_id(), receiver_id=p18.get_id(), weight=users.relation.Relative)
+   admin.accept_relation(p14.get_id(),p18.get_id())
+   
+
+admin.add_relation(sender_id=p15.get_id(), receiver_id=p19.get_id(), weight=users.relation.Sibling)
+   admin.accept_relation(p15.get_id(),p19.get_id())
+   
+
+admin.add_relation(sender_id=p21.get_id(), receiver_id=p20.get_id(), weight=users.relation.Relative)
+   admin.accept_relation(p21.get_id(),p20.get_id())
+   
+
+admin.add_relation(sender_id=p16.get_id(), receiver_id=p22.get_id(), weight=users.relation.Friend)
+   admin.accept_relation(p16.get_id(),p22.get_id())
+   
+
+admin.add_relation(sender_id=p17.get_id(), receiver_id=p23.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p17.get_id(),p23.get_id())
+   
+
+admin.add_relation(sender_id=p18.get_id(), receiver_id=p24.get_id(), weight=users.relation.Sibling)
+   admin.accept_relation(p18.get_id(),p24.get_id())
+   
+
+admin.add_relation(sender_id=p19.get_id(), receiver_id=p25.get_id(), weight=users.relation.Friend)
+   admin.accept_relation(p19.get_id(),p25.get_id())
+   
+
+admin.add_relation(sender_id=p22.get_id(), receiver_id=p26.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p22.get_id(),p26.get_id())
+ 
+
+
+admin.add_relation(sender_id=p23.get_id(), receiver_id=p27.get_id(), weight=users.relation.Sibling)
+   admin.accept_relation(p23.get_id(),p27.get_id())
+   
+
+admin.add_relation(sender_id=p24.get_id(), receiver_id=p28.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p24.get_id(),p28.get_id())
+   
+
+
+admin.add_relation(sender_id=p25.get_id(), receiver_id=p29.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p25.get_id(),p29.get_id())
+   
+
+admin.add_relation(sender_id=p21.get_id(), receiver_id=p30.get_id(), weight=users.relation.Relative)
+   admin.accept_relation(p21.get_id(),p30.get_id())
+   
+
+admin.add_relation(sender_id=p36.get_id(), receiver_id=p26.get_id(), weight=users.relation.Child)
+   admin.accept_relation(p36.get_id(),p26.get_id())
+   
+
+admin.add_relation(sender_id=p37.get_id(), receiver_id=p27.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p37.get_id(),p27.get_id())
+   
+
+
+admin.add_relation(sender_id=p38.get_id(), receiver_id=p28.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p38.get_id(),p28.get_id())
+   
+
+
+admin.add_relation(sender_id=p39.get_id(), receiver_id=p29.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p39.get_id(),p29.get_id())
+   
+
+
+admin.add_relation(sender_id=p31.get_id(), receiver_id=p37.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p31.get_id(),p37.get_id())
+   
+
+
+admin.add_relation(sender_id=p38.get_id(), receiver_id=p32.get_id(), weight=users.relation.Friend)
+   admin.accept_relation(p38.get_id(),p32.get_id())
+   
+
+admin.add_relation(sender_id=p33.get_id(), receiver_id=p39.get_id(), weight=users.relation.Parent)
+   admin.accept_relation(p33.get_id(),p39.get_id())
+   
+
+
+admin.add_relation(sender_id=p34.get_id(), receiver_id=p35.get_id(), weight=users.relation.Friends)
+   admin.accept_relation(p34.get_id(),p35.get_id())
+   
