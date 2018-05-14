df56e26f2a564c8b1b677d1344da5a1dd7a4046e
SohaAlaa <32998187+SohaAlaa@users.noreply.github.com>
2018-05-14 09:57:38 +0200

    Update posts&comm no-ids.py

--- Code/posts&comm no-ids.py
+++ Code/posts&comm no-ids.py
@@ -1,6 +1,8 @@
 admin.add_post(post.Post("Set your goals high, and don’t stop till you get there", 1), 1 )
-admin.Posts[0].add_comment("Motivation will almost always beat mere talent", 2)
-
+admin.Posts[0].add_comment("Motivation will almost always beat mere talent", 3)
+admin.Posts[0].add_comment("Don't cry because it's over, smile because it happened", 4)
+admin.Posts[0].add_comment("Motivation will almost always beat mere talent", 5)
+admin.Posts[0].add_comment("I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best", 6)
 
 
 admin.add_post(post.Post("It’s always too early to quit", 3), 3 )
@@ -9,11 +11,13 @@ admin.Posts[1].add_comment("With self-discipline most anything is possible", 4)
 
 admin.add_post(post.Post("Out of difficulties grow miracles", 5), 5 )
 admin.Posts[2].add_comment("my first comment", 6)
-
+admin.Posts[2].add_comment("my second comment :""D , 8)
+admin.Posts[2].add_comment("my third comment <3 <3 <3 ", 9)
 
 
 admin.add_post(post.Post("The purpose of our lives is to be happy", 7), 7 )
 admin.Posts[3].add_comment("If you have never failed you have never lived", 8)
+admin..Posts[3].add_comment("Be yourself; everyone else is already taken", 10)
 
 
 admin.add_post(post.Post("A jug fills drop by drop", 9), 9 )
@@ -23,33 +27,39 @@ admin.Posts[4].add_comment("The harder I work, the luckier I get", 10)
 
 admin.add_post(post.Post("If there is no struggle, there is no progress", 11), 11 )
 admin.Posts[5].add_comment("Believe and act as if it were impossible to fail", 12)
-
+admin.Posts[5].add_comment("Two things are infinite: the universe and human stupidity; and I'm not sure about the universe", 13)
 
 admin.add_post(post.Post("Dream big and dare to fail", 13), 13 )
 admin.Posts[6].add_comment("Turn your wounds into wisdom", 14)
-
+admin.Posts[6].add_comment("Turn your wounds into wisdom", 15)
 
 
 admin.add_post(post.Post("Great ideas often receive violent opposition from mediocre minds Albert Einstein", 15), 15 )
 admin.Posts[7].add_comment("Don’t count the days, make the days count.” – Muhammad Ali", 16)
+admin.Posts[7].add_comment("So many books, so little time", 19)
 
 
 admin.add_post(post.Post("Either I will find a way, or I will make one", 17), 17 )
 admin.Posts[8].add_comment("After a storm comes a calm", 18)
+admin.Posts[8].add_comment("After a storm comes a calm", 21)
 
 
 
 admin.add_post(post.Post("Your big opportunity may be right where you are now Napoleon Hill",19), 19)
 admin.Posts[9].add_comment("Whoever is happy will make others happy too", 20)
+admin.Posts[9].add_comment("Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind", 22)
 
 
 admin.add_post(post.Post("Never, never, never give up", 21), 21 )
 admin.Posts[10].add_comment("Wherever you go, go with all your heart", 22)
+admin.Posts[10].add_comment("Wherever you go, go with all your heart", 23)
 
 
 
 admin.add_post(post.Post("Wherever you go, go with all your heart", 23), 23 )
 admin.Posts[11].add_comment("Problems are not stop signs, they are guidelines", 24)
+admin.Posts[11].add_comment("A room without books is like a body without a soul", 25)
+admin.Posts[11].add_comment("A room without books is like a body without a soul", 24)
 
 
 admin.add_post(post.Post("Things do not happen. Things are made to happen", 25), 25 )
@@ -63,12 +73,16 @@ admin.Posts[13].add_comment("Do something wonderful, people may imitate it", 28)
 
 admin.add_post(post.Post("Life is short. Build stuff that matters", 29), 29 )
 admin.Posts[14].add_comment("Everything’s got a moral, if only you can find it", 30)
+admin.Posts[14].add_comment("You've gotta dance like there's nobody watching,
+Love like you'll never be hurt,
+Sing like there's nobody listening,
+And live like it's heaven on earth", 36)
 
 
 
 admin.add_post(post.Post("When I let go of what I am, I become what I might be", 31), 31 )
 admin.Posts[15].add_comment("If you want to lift yourself up, lift up someone else", 32)
-
+admin.Posts[15].add_comment("You know you're in love when you can't fall asleep because reality is finally better than your dreams", 35)
 
 admin.add_post(post.Post("Once you choose hope, anything’s possible", 33), 33 )
 admin.Posts[post id16
@@ -76,29 +90,37 @@ admin.Posts[post id16
 
 admin.add_post(post.Post("Live what you love", 35), 35 )
 admin.Posts[17].add_comment("All things are possible if you believe", 36)
+admin.Posts[17].add_comment("You only live once, but if you do it right, once is enough.", 38)
 
 
 admin.add_post(post.Post("What consumes your mind, controls your life", 37), 37 )
 admin.Posts[18].add_comment("You are stronger then you think", 38)
+admin.Posts[18].add_comment("Be the change that you wish to see in the world", 39)
 
 
 
 admin.add_post(post.Post("Hope is a waking dream", 39), 39 )
 admin.Posts[18].add_comment("You miss 100% of the shots you don’t take", 40)
+admin.Posts[18].add_comment("In three words I can sum up everything I've learned about life: it goes on", 43)
 
 
 admin.add_post(post.Post("The mind is everything. What you think you become", 40), 40 )
 admin.Posts[19].add_comment("Dream big and dare to fail", 39)
+admin.Posts[19].add_comment("If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals", 35)
 
 
 
 admin.add_post(post.Post("Do one thing every day that scares you", 33), 33 )
 admin.Posts[20].add_comment("Tough times never last, but tough people do", 32)
+admin.Posts[20].add_comment("Don’t walk in front of me… I may not follow
+Don’t walk behind me… I may not lead
+Walk beside me… just be my friend", 30)
 
 
 admin.add_post(post.Post("Live each day as if your life had just begun", 29), 29 )
 admin.Posts[21].add_comment("Education costs money. But then so does ignorance", 27)
-
+admin.Posts[21].add_comment("If you tell the truth, you don't have to remember anything
+. But then so does ignorance", 26)
 
 
 
