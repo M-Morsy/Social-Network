8939b10bdaeee32dca5e5b87dedc5512781dc9ab
M-Morsy <mohamed.morsey96@gmail.com>
2018-07-01 17:24:10 +0200

    second user interface version v2

--- Code/User_Interface.py
+++ Code/User_Interface.py
@@ -18,6 +18,76 @@ def binary_edge_search (val):
             return mid
     return -1
 
+def option:
+
+def navigate(val, user_num = None):
+    if val == 1:
+        # registeration
+        flag = False
+        print("please enter the following data: ")
+        name = input("name:")
+        while flag is not True:
+            email = input("email: ")
+            email = email.strip()
+            if re.match("\A(?P<name>[\w\-_.]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z", email, re.IGNORECASE):
+                flag = True
+            else:
+                print("Email is invalid")
+
+        flag = False
+        password = input("password: ")
+        age = input("age: ")
+        age = int(age)
+        location = input("location: ")
+        print("Male / Female")
+        while flag is not True:
+            gender = input("gender: ")
+            if gender == "Male" or gender == "male" or gender == "Female" or gender == "female":
+                flag = True
+
+        admin.AddUser(person.Person(name, email, password, age, location, gender))
+
+
+        pass
+    elif val == 2:
+        # timeline
+        print("YOUR TIMELINE: ")
+        print("---------------")
+        admin.get_friends_posts(user_num)
+        pass
+    elif val == 3:
+        # profile, more data
+        print("name     : ", admin.Users[user_num].name)
+        print("email    : ", admin.Users[user_num].email)
+        print("age      : ", admin.Users[user_num].age)
+        print("gender   : ", admin.Users[user_num].gender)
+        print("location : ", admin.Users[user_num].location)
+        pass
+    elif val == 4:
+        # login
+        print("Please enter your email and password")
+        flag = False
+        while flag is not True:
+            email = input("email: ")
+            email = email.strip()
+            if re.match("\A(?P<name>[\w\-_.]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z", email, re.IGNORECASE):
+                flag = True
+            else:
+                print("Email is invalid")
+        password = input("password: ")
+
+        # check if user exists : binary search
+        flag = False
+        user_num = -1
+        for i in range(num):
+            if (admin.Users[i].get_email() == email) and (admin.Users[i].get_password() == password):
+                flag == True
+                user_num = i
+                break
+        return user_num
+        # login
+
+
 # ** normal user ** #
 
 # load graph data
@@ -34,57 +104,29 @@ print("do you need to register ?")
 print("yes : 1    no : 2")
 option = input()
 if option == '1':
-    flag = False
-    print("please enter the following data: ")
-    name = input("name:")
-    while flag is not True:
-        email = input("email: ")
-        email = email.strip()
-        if re.match("\A(?P<name>[\w\-_.]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z",email,re.IGNORECASE):
-            flag = True
-        else:
-            print("Email is invalid")
-
-    flag = False
-    password = input("password: ")
-    age = input("age: ")
-    age = int (age)
-    location = input("location: ")
-    print("Male / Female")
-    while flag is not True:
-        gender = input("gender: ")
-        if gender == "Male" or gender == "male" or gender == "Female" or gender == "female":
-            flag = True
-
-    admin.AddUser(person.Person(name, email, password, age, location, gender))
-
-
+    navigate(1)
     admin.show_graph()
     SL.save(admin)
     # code to login
 
 
-print("Please enter your email and password")
-flag = False
-while flag is not True:
-    email = input("email: ")
-    email = email.strip()
-    if re.match("\A(?P<name>[\w\-_.]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z",email,re.IGNORECASE):
-        flag = True
-    else:
-        print("Email is invalid")
-password = input("password: ")
+user_num = navigate(4)
 
-# check if user exists : binary search
-flag = False
-user_num = -1
-for i in range(num):
-    if (admin.Users[i].get_email() == email) and (admin.Users[i].get_password() == password):
-        flag == True
-        user_num = i
-        break
 # print(user_num) # aurora@gmail.com  aurora;ol7  # 16
+print("Your Profile")
+print("-----------")
+print("name: ", admin.Users[user_num].name)
+print("email: ", admin.Users[user_num].email)
+print(" What do you wish to do ?")
+option = input(""" More Information : 1
+          Timeline : 2
+          """)
 
-print("YOUR TIMELINE: ")
-print("---------------")
-admin.get_friends_posts(user_num)
+if option == '1':
+    navigate(3, user_num)
+    print()
+    navigate(2, user_num)
+elif option == '2' :
+    navigate(2, user_num)
+
+
--- Documentation/documentation.md
+++ Documentation/documentation.md
@@ -1,3 +1,35 @@
-# Documentation
-All documents related to the project
-## Meeting 1 - 26/3/2018
+
+# preface
+------------
+ For humans, development is a necessity not a luxury. The same applies for sports. Analystics on matches and players' performance on field is a hot topic nowadays. 
+ That requires great attention to each player's movements, points of strength, and weakness, and a personalized analysis for each player given match conditions and player's mental and physical state.
+ Obviously, paying attention to all of that is proven to be overwhelming for humans to do anlone, so we are using technology aid to ease that for sports analytic men, coaches, audience, and even sports' game companies.
+ 
+ We chose Football to apply our idea, as it's the most popular sport in Egypt, and Here comes our idea in short:
+ > To use computer vision technology to monitor players performance, and overall match statistics
+
+# Plan
+-------
+We divided our technical plan for computer vision part into four phases:  
+| phase | outcome |  
+| :---: | :---: |  
+| phase one | stitching |  
+| phase two | perfromance boosting |  
+| phase three | tracking players |  
+| phase four | Building final statistical product |  
+
+# Technology Used
+-----
+* Python
+* OpenCV
+* CUDA
+
+
+# Developers
+------
+* Ahmed Fathi
+* Ahmed Naser
+* Ali Badawy
+* Mohamed Morsy
+* Shimaa Hassan
+
